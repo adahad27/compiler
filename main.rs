b6df4cf3bbef02987fa70123863a3ee909c24c01
Abhishek <abhishek.dahad@gmail.com>
2025-06-08 11:17:16 -0400

    Added basic lexing with regex to main.rs Added src_files dir to contain all C files that will be used for testing. Updated .gitignore to ignore debugging files.

--- main.rs
+++ main.rs
@@ -1,4 +1,55 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
 
-    println!("Hello world");
+    let token_list : Vec<String> = lex("src_files/test_1.c");
+    
+    for token in token_list {
+        println!("{}", token);
+    }
+}
+
+
+/* 
+Pass a source file to this function to receive a list of all tokens contained in
+the source file.
+*/
+fn lex(src : &str) -> Vec<String> {
+
+    //Create a dynamic size vector to append tokens from regex into.
+    let mut token_list : Vec<String> = Vec::new();
+
+    //Read the source file here
+    let contents : String = fs::read_to_string(src).expect("Should have been able to read from file");
+
+    let mut regex_list : Vec<regex::Regex> = Vec::new();
+
+    //Regex values of all (so far) allowed tokens
+    let allowed_tokens : Vec<&str> = 
+    vec![
+    "[[:alpha:]]+",
+    ";", 
+    r"\(",
+    r"\)",
+    "[0-9]+"
+    ];
+
+    for token in allowed_tokens {
+        regex_list.push(Regex::new(&token).unwrap());
+    }
+
+    //Regex matching across all constructed regexes.
+    for reg in regex_list {
+        if reg.is_match(&contents) {
+            for token in reg.captures_iter(&contents) {
+                token_list.push(token[0].to_string())
+            }
+        }
+        
+    }
+
+    
+
+    return token_list;
 }
